9a87efeecf2c14e1217feb84d07d8da734475121
ksekmen1 <kaya.sekmen@gmail.com>
2023-04-06 14:47:46 +0200

    ToString til model

--- RentrAPI/Models/RentrItem.cs
+++ RentrAPI/Models/RentrItem.cs
@@ -2,5 +2,15 @@
 {
     public class RentrItem
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Price} {Quantity}";
+        }
+
     }
 }
