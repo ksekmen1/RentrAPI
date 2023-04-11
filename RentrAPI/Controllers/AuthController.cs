27b18d67f0c333c0d51b0bf6fb83de3786058c4c
ksekmen1 <kaya.sekmen@gmail.com>
2023-04-11 21:16:55 +0200

    CreateToken done

--- RentrAPI/Controllers/AuthController.cs
+++ RentrAPI/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using RentrAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace RentrAPI.Controllers
 {
@@ -9,6 +13,12 @@ namespace RentrAPI.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        } 
 
         [HttpPost("register")]
         public ActionResult<User> Register(UserDto request)
@@ -20,6 +30,46 @@ namespace RentrAPI.Controllers
             user.PasswordHash = passwordHash;
 
             return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+            if(user.Username!= request.Username) 
+            {
+                return BadRequest("User not found");
+            }
+
+            if(!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials : creds
+
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
         }
     }
 }
