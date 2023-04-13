b2f8b17d3f6c1562aa2f38092a45ff501826e006
ksekmen1 <kaya.sekmen@gmail.com>
2023-04-13 12:26:44 +0200

    secr

--- RentrAPI/secrets.cs
+++ RentrAPI/secrets.cs
@@ -2,6 +2,6 @@
 {
     public class secrets
     {
-        static string ConnectionString = "Server=tcp:rentr.database.windows.net,1433;Initial Catalog=rentrdb;Persist Security Info=False;User ID=ksek;Password=Vcy22zny;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        static string ConnectionString = "";
     }
 }
